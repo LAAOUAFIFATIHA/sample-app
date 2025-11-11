c9655a947928b2ca5fb0d6558cc1d170d1c277c3
LAAOUAFIFATIHA <laaouafifatiha@gmail.com>
2025-11-11 20:45:10 +0100

    WOOW VERY GOOOD FATIHA

--- sample-app.sh
+++ sample-app.sh
@@ -1,11 +1,15 @@
+
 #!/bin/bash
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-cp app.py tempdir/.
+
+cp  app.py tempdir/.
 cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
@@ -14,3 +18,4 @@ cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
+
